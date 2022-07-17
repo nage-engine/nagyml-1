0cd0a80fc3059210daa1a8daa838ee204f7c25a5
acikek <spiralixp@gmail.com>
2022-07-17 15:30:51 -0700

    fix invalid file jumps

--- src/prompt.nim
+++ src/prompt.nim
@@ -16,9 +16,10 @@ proc validateChoices*(prompt: Prompt, file: string, prompts: Table[string, Table
     if choice.jump.isNone and not choice.ending.isSome:
       return err((index, "Empty jump section in non-ending path"))
     if choice.jump.isSome:
-      if choice.jump.get.file.isSome and not prompts.contains(choice.jump.get.file.get):
-        return err((index, fmt"Jump-file '{choice.jump.get.file.get}' does not exist"))
-      if not prompts.getOrDefault(file).contains(choice.jump.get.prompt):
+      if choice.jump.get.file.isSome:
+        if not prompts.contains(choice.jump.get.file.get):
+          return err((index, fmt"Jump-file '{choice.jump.get.file.get}' does not exist"))
+      elif not prompts.getOrDefault(file).contains(choice.jump.get.prompt):
         return err((index, fmt"Jump-prompt '{choice.jump.get.prompt}' does not exist locally in '{file}'"))
       if prompt.choices.len > 1 and choice.response.isNone:
         return err((index, "Empty response when multiple choices are present"))
